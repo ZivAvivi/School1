9d0d8a0c02247fedcae585beb22cfddec8ff7b9e
Ziv avivi <ziv.avivi.28.13@gmail.com>
2025-10-24 08:44:44 +0300

    droid

---
+++ 2026/computers/android/App2/app/src/main/java/com/example/app2/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.app2;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    Spinner spActions;
+    String[] actions={"?","<",">","="};
+    Context context;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        context = MainActivity.this;
+        spActions = findViewById(R.id.spinner);
+        ArrayAdapter<String> adapter;
+        adapter = new ArrayAdapter<>(context, android.R.layout.simple_spinner_item, actions);
+        spActions.setAdapter(adapter);
+    }
+
+
+
+
+
+
+
+
+
+
+}
