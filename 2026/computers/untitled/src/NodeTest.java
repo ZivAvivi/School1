a5912f52f881c795fc73cc375c70881c29dfea27
Ziv avivi <ziv.avivi.28.13@gmail.com>
2025-11-25 22:35:49 +0200

    LIST

--- 2026/computers/untitled/src/NodeTest.java
+++ 2026/computers/untitled/src/NodeTest.java
@@ -4,7 +4,11 @@ public class NodeTest {
     {
         int [] arr = { 4, 7, 3, 5 };
         Node<Integer> lst1 = build(arr);
+        Node<Integer> lst2 = buildToNum(8);
         show (lst1);
+        show(lst2);
+        System.out.println(isSorted(lst1));
+        System.out.println(isSortedReq(lst1));
     }
     //--- בניית שרשרת מההתחלה לסוף ---
     public static Node<Integer> build (int [] arr)
@@ -34,4 +38,61 @@ public class NodeTest {
         System.out.println("]");
     }
 
+    public static int sumList(Node<Integer> lst){
+        int sum = 0;
+        while (lst != null)
+        {
+            sum += lst.getValue();
+            lst = lst.getNext();
+        }
+
+        return sum;
+    }
+
+        public static Node<Integer> buildToNum (int num)
+        {
+            Node<Integer> lst = new Node<Integer>(0);
+            Node<Integer> pos = lst;
+
+            for (int i = 1 ; i < num ; i++)
+            {
+                pos.setNext (new Node<Integer> (i));
+                pos = pos.getNext();
+            }
+            return lst;
+        }
+
+    public static boolean isNumberInList(int num, Node<Integer> lst){
+        while (lst != null)
+        {
+            if (lst.getValue() == num)
+                return true;
+            lst = lst.getNext();
+        }
+
+        return false;
+    }
+
+    public static boolean isNumberInListReq(int num, Node<Integer> lst){
+        if (lst == null) return false;
+        if (lst.getValue() == num) return true;
+
+        return isNumberInListReq(num, lst.getNext());
+    }
+
+    public static boolean isSorted(Node<Integer> lst){
+        while (lst.getNext() != null)
+        {
+            if (lst.getValue() > lst.getNext().getValue())
+                return false;
+
+            lst = lst.getNext();
+        }
+        return true;
+    }
+
+    public static boolean isSortedReq(Node<Integer> lst){
+        if (lst.getNext() == null) return true;
+        return (lst.getValue() < lst.getNext().getValue() && isSortedReq(lst.getNext()));
+    }
 }
