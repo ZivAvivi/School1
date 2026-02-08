b43eeedb4a35bb9f072729795a234efc3a4cbc06
Ziv avivi <ziv.avivi.28.13@gmail.com>
2026-02-08 12:44:22 +0200

    jhjg

--- 2026/computers/android/rockPaperScissors/app/src/main/java/com/example/rockpaperscissors/logIn.java
+++ 2026/computers/android/rockPaperScissors/app/src/main/java/com/example/rockpaperscissors/logIn.java
@@ -1,8 +1,11 @@
 package com.example.rockpaperscissors;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,8 +15,9 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class logIn extends AppCompatActivity {
 
-    private EditText Email;
-    private EditText Password;
+    private EditText email;
+    private EditText userName;
+    private EditText password;
 
     private Button confirm;
 
@@ -26,6 +30,40 @@ public class logIn extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        // changes the title
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle("RPS");
+        }
+
+        email = findViewById(R.id.editTextTextEmailAddress);
+        password = findViewById(R.id.editTextTextPassword);
+        userName = findViewById(R.id.editTextUserName);
+        confirm = findViewById(R.id.button);
+
+        confirm.setOnClickListener(v -> {
+            String emailText = email.getText().toString();
+            String passwordText = password.getText().toString();
+            String userNameText = userName.getText().toString();
+            if (emailText.isEmpty() || passwordText.isEmpty() || userNameText.isEmpty()) {
+                Toast.makeText(logIn.this, "Please fill in all fields", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            if (!emailText.endsWith("@gmail.com")) {
+                Toast.makeText(logIn.this, "Invalid email", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            SharedPreferences.Editor editor = getSharedPreferences("MyPrefs", MODE_PRIVATE).edit();
+            editor.putString("email", emailText);
+            editor.putString("password", passwordText);
+            editor.putString("userName", userNameText);
+            editor.apply();
+
+            Toast.makeText(logIn.this, "Login successful!", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(logIn.this, MainActivity.class);
+            startActivity(intent);
         });
 
 
